01472b0ba3afd3bae7200fc03613ae5f3cab9836
lieby weintraub <liebahinda@gmail.com>
2019-07-29 22:02:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,13 @@ else
  end 
 
   def show
-    if 
+    if Song.find_by(id: params[:id])
+      @song = Song.find(params[:id])
+    else
+      flash[:alert] = "Song not found"
+      redirect_to artist_songs_path(params[:artist_id])
+    end
+  end
   end
 
   def new
