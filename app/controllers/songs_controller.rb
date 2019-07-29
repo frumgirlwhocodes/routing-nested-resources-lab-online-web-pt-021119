d1da15313633811c1c513551b412095fee2d3c88
lieby weintraub <liebahinda@gmail.com>
2019-07-29 22:00:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,10 @@ class SongsController < ApplicationController
     @songs= Artist.find(params[:artist_id]).songs
   else 
     flash[:alert]="Artist not found"
+    redirect_to artists_path
+  end 
+else 
+ @songs= Song.all
  end
 
   def show
